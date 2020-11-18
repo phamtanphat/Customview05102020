a1d0ef0ac60e6a9b64436deeb2128754acca3644
phatdroid94 <phatdroid94@gmail.com>
2020-11-18 21:05:19 +0700

    attr view

---
+++ app/src/main/java/com/example/customview05102020/EdittextImage.java
@@ -0,0 +1,19 @@
+package com.example.customview05102020;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class EdittextImage extends RelativeLayout {
+    public EdittextImage(Context context) {
+        super(context);
+    }
+
+    public EdittextImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EdittextImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
